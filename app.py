b8b020671efe29763778b42f4baa2d70ced6fb23
Ubuntu <ubuntu@ip-172-31-41-208.ec2.internal>
2019-08-15 12:34:22 +0000

    Have added looping functionality on the Recipes pages. Titles need finishing

--- app.py
+++ app.py
@@ -18,9 +18,14 @@ mongo = PyMongo(app)
 def index():
     return render_template("index.html", recipe=mongo.db.Recipes.find(),
                             cuisine=mongo.db.Cuisines.find())
-
-def recipe():
-    return render_template()
+                            
+@app.route('/recipes/<recipe_id>')
+def recipe(recipe_id):
+    the_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
+    
+    return render_template('recipe.html', recipe = the_recipe, title = the_recipe)
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
