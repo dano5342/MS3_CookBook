c9c458acc2d0b06bc335b367bb9dd2130266cff8
Ubuntu <ubuntu@ip-172-31-41-208.ec2.internal>
2019-08-15 18:48:59 +0000

    added bootstrap login forms, styled and centered. forms.py created for methods of post and get, this needs implementing

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+from forms import LoginForm
 
 app = Flask(__name__)
 
@@ -15,16 +16,22 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 mongo = PyMongo(app)
 
 @app.route('/')
+# Display recipes on the homepage
 def index():
     return render_template("index.html", recipe=mongo.db.Recipes.find(),
                             cuisine=mongo.db.Cuisines.find())
                             
 @app.route('/recipes/<recipe_id>')
+# Take the ObjectID and display the information for the recipe
 def recipe(recipe_id):
     the_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
     
     return render_template('recipe.html', recipe = the_recipe, title = the_recipe)
-    
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
