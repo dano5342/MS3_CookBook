004c09ff4d59cd9e273ce7c954fa4d8ad47e1be9
dano5342 <ellis.daniel0@gmail.com>
2019-09-03 11:16:58 +0000

    have added if/else statements to remove the syntax bug appearing in the rest of the main site, as the function was only intended for add/edit recipe pages for field creation

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,37 +1,50 @@
-let ingredientCounter = ($(".ingredients").length > 1) ? $(".ingredients").length : null;
-
-/* Add new ingredient line */
-$(".i-add").click(function() {
-    /* append form, insert before add/remove btns and clear existing values */
-    $(".ingredients:last").append("<input type='text' name='ingredients' class='ingredients' placeholder='One ingredient per box!'/>")
-        .insertBefore(".i-add")
-        .find("input[type='text']").val("");
-    ingredientCounter += 1;
-});
-/* Remove last ingredients line */
-$(".i-remove").click(function() {
-    if (ingredientCounter > 1) {
-        $(".ingredients:last").remove();
-        ingredientCounter -= 1;
-    }
-});
+var isOnRecipe = document.getElementsByClassName(".ingredients");
+var methodOnRecipe = document.getElementsByClassName(".methods");
 
 
-let methodCount = ($(".methods").length > 1) ? $(".methods").length : null;
+if (isOnRecipe.length > 0) {
 
-/* Add new line */
-$(".m-add").click(function() {
-    /* append form, insert before add/remove btns and clear existing values */
-    $(".methods:last").append("<input type='text' name='methods' class='methods' required placeholder='Boil water and drop in spaghetti, add balsamic vinegar to bring out a nice tangy flavour...'/>")
-        .insertBefore(".m-add")
-        .find("input[type='text']").val("");
-    methodCount += 1;
-});
+}
+else {
+    let ingredientCounter = $(".ingredients").length
 
-/* Remove last line */
-$(".m-remove").click(function() {
-    if (methodCount > 1) {
-        $(".methods:last").remove();
-        methodCount -= 1;
-    }
-});
+    /* Add new ingredient line */
+    $(".i-add").click(function() {
+        /* append form, insert before add/remove btns and clear existing values */
+        $(".ingredients:last").append("<input type='text' name='ingredients' class='ingredients' placeholder='One ingredient per box!'/>")
+            .insertBefore(".i-add")
+            .find("input[type='text']").val("");
+        ingredientCounter += 1;
+    });
+    /* Remove last ingredients line */
+    $(".i-remove").click(function() {
+        if (ingredientCounter > 1) {
+            $(".ingredients:last").remove();
+            ingredientCounter -= 1;
+        }
+    });
+}
+
+if (methodOnRecipe.length > 0) {
+
+}
+else {
+    let methodCount = $(".methods").length
+
+    /* Add new line */
+    $(".m-add").click(function() {
+        /* append form, insert before add/remove btns and clear existing values */
+        $(".methods:last").append("<input type='text' name='methods' class='methods' required placeholder='Boil water and drop in spaghetti, add balsamic vinegar to bring out a nice tangy flavour...'/>")
+            .insertBefore(".m-add")
+            .find("input[type='text']").val("");
+        methodCount += 1;
+    });
+
+    /* Remove last line */
+    $(".m-remove").click(function() {
+        if (methodCount > 1) {
+            $(".methods:last").remove();
+            methodCount -= 1;
+        }
+    });
+}
