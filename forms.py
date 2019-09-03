d89907f05b4ff35965ab83b347fbb154f45b2430
dano5342 <ellis.daniel0@gmail.com>
2019-09-03 11:08:42 +0000

    forms now pep8 compliant

--- forms.py
+++ forms.py
@@ -1,21 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField, FieldList, FormField
-from wtforms.validators import DataRequired, Length, Optional, InputRequired, EqualTo
+from wtforms import (StringField, PasswordField, BooleanField, SubmitField,
+                     TextAreaField, IntegerField, FieldList, FormField)
+from wtforms.validators import (DataRequired, Length, Optional,
+                                InputRequired, EqualTo)
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=16)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=100)])
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=5, max=16)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                             Length(min=5, max=100)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-    
+
+
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=16)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=100), EqualTo('confirm', message="Passwords must match")])
+    username = StringField('Username', validators=[DataRequired(),
+                           Length(min=5, max=16)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                             Length(min=5, max=100),
+                             EqualTo('confirm',
+                             message="Passwords must match")])
     confirm = PasswordField('Confirm Password')
     submit = SubmitField('Register')
 
 
-## Custom forms for single input fields. ARRAYS had to be done manually.
+# Custom forms for single input fields. ARRAYS had to be done manually.
 
 class RecipeForm(FlaskForm):
     recipe_name = StringField('Recipe Name:', [InputRequired()])
@@ -26,4 +36,3 @@ class RecipeForm(FlaskForm):
     cook_time = IntegerField('Cooking Time:', [InputRequired()])
     img_url = StringField('Got a photo link?:', [Optional()])
     submit = SubmitField('Add Recipe')
-
